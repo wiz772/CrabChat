109b4c97a44872073dba3d9882a10069bc82d37b
wiz <guerand.julien77@gmail.com>
2026-04-13 12:01:59 +0200

    refactoring: improved architecture and ownership handling

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,33 @@ const LISTENER_ADDR: &str ="127.0.0.1:8080";
 
 fn handle_client(mut client: TcpStream){ 
 
-} 
+}
+
+fn display_connection_log(stream: &TcpStream){
+    match stream.peer_addr() {
+        Ok(addr) =>  println!("Connected: {}", addr),
+        Err(e) => println!("[ERROR] Could not get client address: {}", e),
+    }
+}
+
+fn dispatch_client(mut stream: TcpStream){
+    thread::spawn(move|| {
+        handle_client(stream);
+    }); 
+}
+
+fn handle_incoming_connection(mut stream: TcpStream){
+    display_connection_log(&stream);
+    dispatch_client(stream);
+}
 
 fn start_listening(){ 
     let listener = TcpListener::bind(LISTENER_ADDR).unwrap();
     println!("Server started."); 
     for stream in listener.incoming(){ 
         match stream{
-            Ok(mut stream) => {
-                match stream.peer_addr() {
-                    Ok(addr) =>  println!("Connected: {}", addr),
-                    Err(e) => println!("Could not get client address: {}", e),
-                }
-                // créer thread par client
-                thread::spawn(move ||{
-                    handle_client(stream);
-                });            
+            Ok(stream) => {
+                handle_incoming_connection(stream);         
             }
 
             Err(e) => {
