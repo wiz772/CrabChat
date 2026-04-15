f6965635ca9007a958cad24f23f84e285ac2db6b
wiz <guerand.julien77@gmail.com>
2026-04-15 09:43:07 +0200

    improved client_session.rs

--- src/server/client_session.rs
+++ src/server/client_session.rs
@@ -1,12 +1,25 @@
+use std::collections::HashMap;
 use std::net::TcpStream;
+use std::sync::{Arc, Mutex};
+use std::usize;
+
+type Sessions = Arc<Mutex<HashMap<usize, ClientSession>>>;
 
 pub struct ClientSession {
-
+    id: usize,
     stream: TcpStream, 
     username: Option<String>
 
 }
 
-// impl Client {
+impl ClientSession {
 
-// }
+    pub fn new(id: usize, stream: TcpStream) -> Result<Self, String> {
+        Ok(ClientSession{ id, stream, username: None})
+    }
+
+    pub fn set_username(&mut self, new_username: String) {
+        self.username = Some(new_username);
+    }
+
+}
